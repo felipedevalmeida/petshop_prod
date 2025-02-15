e630235f8ad465ccb64a5719890fadf47b6b3dd1
felipedevalmeida <felipe@felipealmeidadev.com.br>
2025-02-15 17:56:59 -0300

    Update conexao.php
    Printando eventuais erros na tela.

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,6 +1,10 @@
 <?php
 // conexao/conexao.php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servidor = "br536.hostgator.com.br";
 $usuario_bd = "fel28016_felipealmeidadev";
 $senha_bd   = "seis@91seise23";
